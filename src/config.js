f8ab593d90fd570f5cfc49e68e9c25244c5c947b
ksftso0x <kiryas1101@gmail.com>
2023-12-30 23:30:36 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  chainId: 4,
+  contractAddress: "0x628CB4b87B6B71455074f66C73B8934706dcB238",
+  scanLink:
+    "https://rinkeby.etherscan.io/address/0x628CB4b87B6B71455074f66C73B8934706dcB238",
+};
